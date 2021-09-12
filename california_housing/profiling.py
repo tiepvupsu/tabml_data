2559a316fbf6607205928cbcf4e852d119b82363
Tiep Vu <vuhuutiep@gmail.com>
2021-09-11 23:56:35 -0700

    add profiling for housing

---
+++ california_housing/profiling.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+def profile(input_file, output_html):
+    train_df = pd.read_csv(input_file)
+    prof = ProfileReport(train_df)
+    prof.to_file(output_file=output_html)
+
+
+profile("housing.csv", "housing.html")
